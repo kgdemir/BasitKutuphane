3c0629fc0c96f2f2755ba947c7ea1040d87a3c99
Kerim Gökhan DEMİR <kgdemir@gmail.com>
2020-12-17 19:54:52 +0300

    Update Sorgular.sql

--- Sorgular.sql
+++ Sorgular.sql
@@ -10,9 +10,22 @@ SELECT * FROM Yazar
 --Kitaplar
 SELECT * FROM Kitap
 
---JOINlenmiş halleri
+--Öğrenciler
+SELECT * FROM Ogrenci
+
+--İşlemler
+SELECT * FROM Islem
+
+--Kitap Detayları
 SELECT K.Id, K.Adi, YE.Yayinevi, Y.Yazar, K.Basimyili, T.Tur, K.ISBN FROM Kitap K
 LEFT JOIN Yayinevi YE ON K.Yayinevi=YE.Id
 LEFT JOIN Yazar Y ON K.Yazar=Y.Id
 LEFT JOIN Turler T ON K.Tur=T.Id
 
+--İşlem Detayları
+SELECT I.Id, I.BaslangicTarih, I.BitisTarih, O.AdSoyad, O.KimlikNo, O.OgrenciNo, K.Adi, YE.Yayinevi, Y.Yazar, K.Basimyili, T.Tur, K.ISBN FROM Islem I
+LEFT JOIN Kitap K ON I.KitapId=K.Id
+LEFT JOIN Yayinevi YE ON K.Yayinevi=YE.Id
+LEFT JOIN Yazar Y ON K.Yazar=Y.Id
+LEFT JOIN Turler T ON K.Tur=T.Id
+LEFT JOIN Ogrenci O ON I.OgrenciId=O.Id
